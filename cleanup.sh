8b2036d29db1e12647440aaf2c6fe4188f22d9df
Christian Schulz <christian.schulz@kit.edu>
2013-11-07 15:53:02 +0100

    minor modifications for cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -3,8 +3,6 @@
 
 scons program=kaffpa variant=optimized -j 4 -c 
 scons program=kaffpaE variant=optimized -j 4 -c 
-scons program=buffoon variant=optimized -j 4 -c
-scons program=kabar variant=optimized -j 4 -c
 scons program=partition_to_vertex_separator variant=optimized -j 4 -c
 scons program=library variant=optimized -j 4 -c
 
