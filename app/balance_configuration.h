c2508ad2cfa3e25dbc4dd34b91ccec80aa249c7d
Christian Schulz <christian.schulz@kit.edu>
2014-11-17 15:25:26 +0100

    switched to GPLv2

--- app/balance_configuration.h
+++ app/balance_configuration.h
@@ -3,11 +3,11 @@
  *
  * Source of KaHIP -- Karlsruhe High Quality Partitioning 
  ******************************************************************************
- * Copyright (C) 2014 Christian Schulz <christian.schulz@kit.edu>
+ * Copyright (C) 2013-2015 Christian Schulz <christian.schulz@kit.edu>
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
+ * Software Foundation, either version 2 of the License, or (at your option)
  * any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
--- lib/partition/uncoarsening/refinement/quotient_graph_refinement/flow_refinement/flow_solving_kernel/timer.cpp
+++ lib/partition/uncoarsening/refinement/quotient_graph_refinement/flow_refinement/flow_solving_kernel/timer.cpp
@@ -4,16 +4,15 @@
  * Source of KaHIP -- Karlsruhe High Quality Partitioning.
  * based on HiPr  -- Copyright 1995, 2000 by IG Systems, Inc., igsys@eclipse.net 
  * "Maximum flow - highest lavel push-relabel algorithm"
- * Copyright for this release gpl 3.0 granted by Andrew Goldberg
  *
  * comment: we used the implementation of hipr and put them into a header file here
  *
  ******************************************************************************
- * Copyright (C) 2013 Christian Schulz <christian.schulz@kit.edu>
+ * Copyright (C) 2013-2015 Christian Schulz <christian.schulz@kit.edu>
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
+ * Software Foundation, either version 2 of the License, or (at your option)
  * any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
