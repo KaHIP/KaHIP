ca2d91902f2a761570579c46647fc14958193695
Christian Schulz <christian.schulz@kit.edu>
2015-08-04 16:26:49 +0200

    flexible ratings in vcycles

--- lib/partition/graph_partitioner.cpp
+++ lib/partition/graph_partitioner.cpp
@@ -190,17 +190,19 @@ void graph_partitioner::single_run( PartitionConfig & config, graph_access & G) 
 
                                 graph_hierarchy hierarchy;
 
-                                //if( config.mode_node_separators ) {
-                                //if( i % 4 == 1 ) {
-                                //config.edge_rating = SEPARATOR_MAX;
-                                //} else if ( i % 4 == 2 ) {
-                                //config.edge_rating = SEPARATOR_LOG;
-                                //} else if ( i % 4 == 3 ) {
-                                //config.edge_rating = SEPARATOR_ADDX;
-                                //} else if ( i % 4 == 0 ) {
-                                //config.edge_rating = SEPARATOR_MULTX;
-                                //}
-                                //}
+                                if( config.mode_node_separators ) {
+                                        if( i % 6 == 2 ) {
+                                                config.edge_rating = SEPARATOR_MULTX;
+                                        } else if ( i % 6 == 3 ) {
+                                                config.edge_rating = SEPARATOR_ADDX;
+                                        } else if ( i % 6 == 4 ) {
+                                                config.edge_rating = SEPARATOR_R8;
+                                        } else if ( i % 6 == 5 ) {
+                                                config.edge_rating = SEPARATOR_LOG;
+                                        } else if ( i % 6 == 0 ) {
+                                                config.edge_rating = SEPARATOR_MAX;
+                                        }
+                                }
                                 coarsen.perform_coarsening(config, G, hierarchy);
                                 init_part.perform_initial_partitioning(config, hierarchy);
                                 uncoarsen.perform_uncoarsening(config, hierarchy);
