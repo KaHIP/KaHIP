e6e50103bba770f9b6f4b4dfd7f73b6f0694a90a
Christian Schulz <christian.schulz@kit.edu>
2015-08-07 11:03:02 +0200

    starting to implement FM local search

--- lib/partition/uncoarsening/separator/vertex_separator_algorithm.h
+++ lib/partition/uncoarsening/separator/vertex_separator_algorithm.h
@@ -20,6 +20,7 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
 
+
 #ifndef VERTEX_SEPARTATOR_ALGORITHM_XUDNZZM8
 #define VERTEX_SEPARTATOR_ALGORITHM_XUDNZZM8
 
