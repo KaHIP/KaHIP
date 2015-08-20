45d68b059886fa89bbe2e5135b8eb77ef9ccd8c6
Christian Schulz <christian.schulz@kit.edu>
2015-08-20 21:40:15 +0200

    more speedup

--- lib/partition/initial_partitioning/initial_node_separator.cpp
+++ lib/partition/initial_partitioning/initial_node_separator.cpp
@@ -32,13 +32,6 @@ NodeWeight initial_node_separator::single_run( const PartitionConfig & config, g
         partition_config.mode_node_separators    = false;
         partition_config.global_cycle_iterations = 1;
         partition_config.repetitions             = 1;
-        //partition_config.edge_rating             = config.sep_edge_rating_during_ip;
-
-
-        //if( rnd == 0 ) partition_config.edge_rating = SEPARATOR_MULTX;
-        //if( rnd == 1 ) partition_config.edge_rating = SEPARATOR_ADDX;
-        //if( rnd == 2 ) partition_config.edge_rating = SEPARATOR_MAX;
-        //if( rnd == 3 ) partition_config.edge_rating = SEPARATOR_LOG;
 
         //computing a partition
         partitioner.perform_partitioning(partition_config, G);
@@ -57,31 +50,6 @@ NodeWeight initial_node_separator::single_run( const PartitionConfig & config, g
                 vsa.compute_vertex_separator_simple(partition_config, G, boundary, separator);
         }
  
-	//fm_ns_local_search fmnsls;
-	//std::vector< NodeWeight > block_weights(3,0);
-	//forall_nodes(G, node) {
-		//if( G.getPartitionIndex(node) == 0) {
-			//block_weights[0] += G.getNodeWeight(node);
-		//} else if( G.getPartitionIndex(node) == 1 ) {
-			//block_weights[1] += G.getNodeWeight(node);
-		//} else {
-			//block_weights[2] += G.getNodeWeight(node);
-		//}
-	//} endfor
-
-	//if(block_weights[0] > block_weights[1]) {
-		//fmnsls.perform_refinement(partition_config, G, true, 1);
-	//} else {
-		//fmnsls.perform_refinement(partition_config, G, true, 0);
-	//}
-	//fmnsls.perform_refinement(partition_config, G);
-	//separator.clear();
-	//forall_nodes(G, node) {
-		//if(G.getPartitionIndex(node) == 2) {
-			//separator.push_back(node);
-		//}
-	//} endfor
-
         std::vector<NodeID> output_separator;
         //improve the separator using flow based techniques
         vsa.improve_vertex_separator(partition_config, G, separator, output_separator);
@@ -97,6 +65,8 @@ void initial_node_separator::compute_node_separator( const PartitionConfig & con
         std::vector< NodeID > best_separator(G.number_of_nodes(),0);
         NodeWeight best_separator_size = std::numeric_limits< NodeWeight >::max();
 
+	int unsucc_counter = 0;
+
         for( int i  = 0; i  < config.max_initial_ns_tries; i++) {
                 NodeWeight cur_separator_size = single_run(config, G);
                 if(cur_separator_size < best_separator_size) {
@@ -106,7 +76,14 @@ void initial_node_separator::compute_node_separator( const PartitionConfig & con
                         best_separator_size = cur_separator_size;
                 
                         std::cout <<  "improved initial separator size " <<  cur_separator_size  << std::endl;
-                }
+			unsucc_counter = 0;
+                } else {
+			unsucc_counter++;
+		}
+
+		if( config.faster_ns && unsucc_counter >= 5) {
+			break;
+		}
         }
 
         forall_nodes(G, node) {
