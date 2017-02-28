5b1bc58b71ec30a30eceb78307db790e8ca9fd00
Christian Schulz <christian.schulz@kit.edu>
2017-02-28 11:01:03 +0100

    adapting license to GPL-2

--- parallel/parallel_src/compile_all.sh
+++ parallel/parallel_src/compile_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for program in parhip generate_ba edge_list_to_metis_graph friendster_list_to_metis_graph generate_rgg graph2binary graph2binary_external readbgf toolbox; do 
+for program in parhip edge_list_to_metis_graph friendster_list_to_metis_graph graph2binary graph2binary_external readbgf toolbox; do 
 scons program=$program variant=optimized -j 16
 if [ "$?" -ne "0" ]; then 
         echo "compile error in $program. exiting."
