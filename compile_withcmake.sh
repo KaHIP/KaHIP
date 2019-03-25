63b7dc0a6d69761fd13f7450f5b527b4988f611a
Schulz Christian <christians84cs@repeal2.taa.univie.ac.at>
2019-03-25 15:14:10 +0100

    cmake compiler wrapper

---
+++ compile_withcmake.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+NCORES=4
+unamestr=`uname`
+if [[ "$unamestr" == "Linux" ]]; then
+        NCORES=`grep -c ^processor /proc/cpuinfo`
+fi
+
+if [[ "$unamestr" == "Darwin" ]]; then
+        NCORES=`sysctl -n hw.ncpu`
+fi
+
+rm -rf deploy
+rm -rf build
+mkdir build
+cd build 
+cmake ../
+make -j $NCORES
+cd ..
+
+mkdir deploy
+cp ./build/kaffpa deploy/
+cp ./build/evaluator deploy/
+cp ./build/label_propagation deploy/
+cp ./build/kaffpaE deploy/
+cp ./build/graphchecker deploy/
+cp ./build/partition_to_vertex_separator deploy/
+cp ./build/node_separator deploy/
+cp ./build/edge_partitioning deploy/
+cp ./build/parallel/parallel_src/dsp* ./deploy/distributed_edge_partitioning
+cp ./build/parallel/parallel_src/g* ./deploy
+cp ./build/parallel/parallel_src/parhip* ./deploy/parhip
+cp ./build/parallel/parallel_src/toolbox* ./deploy/
+cp ./interface/kaHIP_interface.h deploy/
+cp ./parallel/parallel_src/interface/parhip_interface.h deploy/
+
+
