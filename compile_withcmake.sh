c4bb90943d0546eb3a197ba6767f2b670600b29d
Schulz Christian <christians84cs@repeal2.taa.univie.ac.at>
2019-03-26 10:21:24 +0100

    final touches on cmake

--- compile_withcmake.sh
+++ compile_withcmake.sh
@@ -27,10 +27,12 @@ cp ./build/graphchecker deploy/
 cp ./build/partition_to_vertex_separator deploy/
 cp ./build/node_separator deploy/
 cp ./build/edge_partitioning deploy/
+cp ./build/libinterface_static.a deploy/libkahip.a
 cp ./build/parallel/parallel_src/dsp* ./deploy/distributed_edge_partitioning
 cp ./build/parallel/parallel_src/g* ./deploy
 cp ./build/parallel/parallel_src/parhip* ./deploy/parhip
 cp ./build/parallel/parallel_src/toolbox* ./deploy/
+cp ./build/parallel/parallel_src/libparhip_inter*.a deploy/libparhip.a
 cp ./interface/kaHIP_interface.h deploy/
 cp ./parallel/parallel_src/interface/parhip_interface.h deploy/
 
