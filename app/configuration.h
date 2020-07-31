850b29f37017059e414dd7f5668c2e85b9395056
Christian Schulz <christian.schulz@univie.ac.at>
2020-07-31 13:15:43 +0200

    default parameters in configuration.h

--- app/configuration.h
+++ app/configuration.h
@@ -468,8 +468,24 @@ inline void configuration::standard( PartitionConfig & partition_config ) {
         partition_config.distances.push_back(10);
         partition_config.distances.push_back(100);
 
+        partition_config.convergence_factor = 1;
+        partition_config.reduction_order = {simplicial_nodes,
+                indistinguishable_nodes,
+                //twins,
+                //path_compression,
+                degree_2_nodes,
+                triangle_contraction};
 
+        partition_config.dissection_rec_limit = 120;
+        partition_config.disable_reductions = false;
 
+        partition_config.ilp_min_gain = -1;
+        partition_config.ilp_bfs_depth = 2;
+        partition_config.ilp_overlap_presets = OverlapPresets::NOEQUAL;
+
+        partition_config.ilp_limit_nonzeroes = 5000000;
+        partition_config.ilp_overlap_runs = 3;
+        partition_config.ilp_timeout = 7200;
 }
 
 inline void configuration::standardsnw( PartitionConfig & partition_config ) {
