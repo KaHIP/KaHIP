174719ed32cd3d56939a645078a0be64282ce218
tzovas <tzovas.charilaos@hu-berlin.de>
2020-09-11 10:01:17 +0200

    tabs formatting

--- parallel/modified_kahip/lib/partition/uncoarsening/refinement/quotient_graph_refinement/flow_refinement/two_way_flow_refinement.cpp
+++ parallel/modified_kahip/lib/partition/uncoarsening/refinement/quotient_graph_refinement/flow_refinement/two_way_flow_refinement.cpp
@@ -59,8 +59,8 @@ EdgeWeight two_way_flow_refinement::iterativ_flow_iteration(PartitionConfig & co
 
         if(lhs_pq_start_nodes.size() == 0 or rhs_pq_start_nodes.size() == 0) return 0; // nothing to refine
 
-        //std::cout << "\n\n\n" << lhs_part_weight << " __ " << rhs_part_weight << " ## " << config.upper_bound_partition << std::endl;
-        ASSERT_TRUE(lhs_part_weight < config.upper_bound_partition && rhs_part_weight < config.upper_bound_partition);
+//printf( "lhs= %i, rhs= %i, upper bound= %i \n", lhs_part_weight, rhs_part_weight, config.upper_bound_partition );
+//ASSERT_TRUE(lhs_part_weight < config.upper_bound_partition && rhs_part_weight < config.upper_bound_partition);
 
         PartitionID lhs = refinement_pair->lhs;
         PartitionID rhs = refinement_pair->rhs;
