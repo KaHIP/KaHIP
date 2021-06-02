2e6c6965bea61d279a0f52aec0ad29e34f644733
Mark <krnejchik@mail.ru>
2021-06-02 20:11:04 +0300

    Optimize linear_probing_hashmap for O(1) clear

--- parallel/parallel_src/lib/data_structure/linear_probing_hashmap.h
+++ parallel/parallel_src/lib/data_structure/linear_probing_hashmap.h
@@ -33,18 +33,16 @@ public:
                 m_real_size = m_size + max_size*1.1; // allow some slack in the end so that we do not need to handle special cases
 
                 m_internal_map.resize(m_real_size);
+                m_used.resize(m_real_size, NOT_CONTAINED);
+
+                CONTAINED_FLAG = NOT_CONTAINED - 1;
                 m_last_request = NOT_CONTAINED;
                 m_last_pos     = NOT_CONTAINED;
         }
 
         void clear() {
-                while( m_contained_key_positions.size() > 0 ) {
-                        NodeID cur_key_position = m_contained_key_positions.top();
-                        m_contained_key_positions.pop();
+                --CONTAINED_FLAG;
 
-                        m_internal_map[cur_key_position].key   = NOT_CONTAINED;
-                        m_internal_map[cur_key_position].value = 0;
-                }
                 m_last_request = NOT_CONTAINED;
         }
 
@@ -52,32 +50,32 @@ public:
         NodeID contains( NodeID node ) {
                 if( m_last_request == node ) return true;
                 NodeID hash_value = hash(node);
-                for( NodeID i = hash_value; i < m_real_size; i++) {
-                        if( m_internal_map[i].key == node || m_internal_map[i].key == NOT_CONTAINED) {
+                for( NodeID i = hash_value; i < m_real_size; ++i) {
+                        if( m_internal_map[i].key == node || m_used[i] != CONTAINED_FLAG ) {
                                 hash_value = i;
                                 break;
                         }
                 }
 
-                return m_internal_map[hash_value].key == node;
+                return m_internal_map[hash_value].key == node && m_used[hash_value] == CONTAINED_FLAG;
         }
 
         // find table position or the next free table position if it is not contained
-        NodeID find( NodeID node ) {
+        inline NodeID find( NodeID node ) {
                 if( m_last_request == node ) return m_last_pos;
 
                 NodeID hash_value = hash(node);
-                for( NodeID i = hash_value; i < m_real_size; i++) {
-                        if( m_internal_map[i].key == node || m_internal_map[i].key == NOT_CONTAINED) {
+                for( NodeID i = hash_value; i < m_real_size; ++i) {
+                        if( m_internal_map[i].key == node || m_used[i] != CONTAINED_FLAG ) {
                                 hash_value = i;
                                 break;
                         }
                 }
 
-                if( m_internal_map[hash_value].key == NOT_CONTAINED ) {
+                if( m_used[hash_value] != CONTAINED_FLAG ) {
+                        m_used[hash_value] = CONTAINED_FLAG;
                         m_internal_map[hash_value].key = node;
                         m_internal_map[hash_value].value = 0;
-                        m_contained_key_positions.push(hash_value);
                 }
 
                 m_last_request = node;
@@ -96,12 +94,14 @@ public:
 
         std::vector< KeyValuePair > * internal_access() {return & m_internal_map; }
 private:
+        NodeID CONTAINED_FLAG = NOT_CONTAINED;
+
         NodeID m_size; 
         NodeID m_real_size; 
         NodeID m_last_pos;
         NodeID m_last_request;
         std::vector< KeyValuePair > m_internal_map;
-        std::stack< NodeID > m_contained_key_positions;
+        std::vector< NodeID > m_used;
 };
 
 
