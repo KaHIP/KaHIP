b3059cccb2afe89ac4e2010ff994e459b6423278
Christian Schulz <christian.schulz.phone@gmail.com>
2024-12-19 17:08:14 +0100

    .

--- lib/version.h
+++ lib/version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifdef DETERMINISTIC_PARHIP
-const std::string KAHIPVERSION = "v3.14 DETERMINISTIC_PARHIP";
+const std::string KAHIPVERSION = "v3.17 DETERMINISTIC_PARHIP";
 #else 
-const std::string KAHIPVERSION = "v3.14";
+const std::string KAHIPVERSION = "v3.17";
 #endif
